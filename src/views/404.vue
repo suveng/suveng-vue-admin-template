d02401f87a8a7b3a2b51f4ef6fec013852d2777e
suveng <suveng@163.com>
2019-02-21 11:02:20 +0800

    改造404

--- src/views/404.vue
+++ src/views/404.vue
@@ -9,11 +9,11 @@
       </div>
       <div class="bullshit">
         <div class="bullshit__oops">OOPS!</div>
-        <div class="bullshit__info">版权所有
-          <a class="link-type" href="https://wallstreetcn.com" target="_blank">华尔街见闻</a>
+        <div class="bullshit__info">邮 箱：
+          <p class="link-type" href="https://suveng@163.com" target="_blank">suveng@163.com</p>
         </div>
         <div class="bullshit__headline">{{ message }}</div>
-        <div class="bullshit__info">请检查您输入的网址是否正确，请点击以下按钮返回主页或者发送错误报告</div>
+        <div class="bullshit__info">请检查您输入的网址是否正确，请点击以下按钮返回主页！或者 发送邮件给管理员!</div>
         <a href="" class="bullshit__return-home">返回首页</a>
       </div>
     </div>
