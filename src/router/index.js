188078a907f7f87a5913330d1ff158fbc58ba1c7
suveng <suveng@163.com>
2019-03-18 19:01:16 +0800

    路劲对上了

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+/* Layout */
+import Layout from '../views/layout/Layout'
 
 // in development-env not use lazy-loading, because lazy-loading too many pages will cause webpack hot update too slow. so only in production use lazy-loading;
 // detail: https://panjiachen.github.io/vue-element-admin-site/#/lazy-loading
 
 Vue.use(Router)
 
-/* Layout */
-import Layout from '../views/layout/Layout'
-
 /**
-* hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
-* alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
-*                                if not set alwaysShow, only more than one route under the children
-*                                it will becomes nested mode, otherwise not show the root menu
-* redirect: noredirect           if `redirect:noredirect` will no redirect in the breadcrumb
-* name:'router-name'             the name is used by <keep-alive> (must set!!!)
-* meta : {
+ * hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
+ * alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
+ *                                if not set alwaysShow, only more than one route under the children
+ *                                it will becomes nested mode, otherwise not show the root menu
+ * redirect: noredirect           if `redirect:noredirect` will no redirect in the breadcrumb
+ * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+ * meta : {
     title: 'title'               the name show in subMenu and breadcrumb (recommend set)
     icon: 'svg-name'             the icon show in the sidebar
     breadcrumb: false            if false, the item will hidden in breadcrumb(default is true)
   }
-**/
+ **/
 export const constantRouterMap = [
   { path: '/login', component: () => import('@/views/login/index'), hidden: true },
   { path: '/404', component: () => import('@/views/404'), hidden: true },
@@ -37,7 +36,22 @@ export const constantRouterMap = [
       component: () => import('@/views/dashboard/index')
     }]
   },
-
+  // todo://侧边栏在这里加
+  {
+    path: '/user',
+    component: Layout,
+    redirect: '/user/index',
+    name: 'User',
+    meta: { title: '用户模块', icon: 'user' },
+    children: [
+      {
+        path: 'index',
+        name: 'Index',
+        component: () => import('@/views/user/index'),
+        meta: { title: '用户管理', icon: 'user' }
+      }
+    ]
+  },
   {
     path: '/example',
     component: Layout,
@@ -123,6 +137,7 @@ export const constantRouterMap = [
           }
         ]
       },
+
       {
         path: 'menu2',
         component: () => import('@/views/nested/menu2/index'),
---
+++ src/views/user/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="user">
+    这是我第一个vue模块
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'User'
+}
+</script>
+
+<style scoped>
+
+</style>
