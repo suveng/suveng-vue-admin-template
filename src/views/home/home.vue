798a4053772ac7ee525de79dcbe093ccaca89b7e
suveng <suveng@163.com>
2019-03-18 20:50:53 +0800

    home page 主页模块

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,24 +1,17 @@
 <template>
-  <div class="suveng" style="height: 100% ; width: 100%">
-    <iframe src="https://suveng.github.io/blog/about.me.html" onload="changeFrameHeight()" frameborder="0" scrolling="no" style="height: 100%;width: 100%"/>
+  <div class="suveng" >
+    <iframe width="100%" height="100%" src="https://suveng.github.io/blog/about.me.html" frameborder="0" scrolling="yes" />
   </div>
 </template>
 <script>
 export default {
   name: 'Home'
 }
-function changeFrameHeight() {
-  var ifm = document.getElementById('iframepage')
-  ifm.height = document.documentElement.clientHeight
-}
-window.onresize = function() {
-  changeFrameHeight()
-}
 </script>
 
 <style scoped>
 .suveng{
   width: 100%;
-  height: 100%;
+  height: 94.6vh;
 }
 </style>
