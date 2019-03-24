ef5be4784a10dded515d7efe13c256f5a3103ffd
suveng <suveng@163.com>
2019-03-24 19:13:17 +0800

    解决https

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://132.232.71.94:9000"',
+  BASE_API: '"https://132.232.71.94"',
 })
